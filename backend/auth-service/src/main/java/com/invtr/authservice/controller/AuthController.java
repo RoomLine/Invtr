e62cfa3d626254e9340f18aed9c51c1718d555d4
GamingDriver <yotov180@gmail.com>
2026-03-12 23:41:16 +0200

    AuthService controller with basic endpoint

--- backend/auth-service/src/main/java/com/invtr/authservice/controller/AuthController.java
+++ backend/auth-service/src/main/java/com/invtr/authservice/controller/AuthController.java
@@ -1,4 +1,13 @@
 package com.invtr.authservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "if you're seeing this, the app(AuthController) is working";
+    }
 }

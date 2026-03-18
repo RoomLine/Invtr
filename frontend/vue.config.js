d9ed2e70cac41cdb93b2d91488bd2c13fadccc84
boored342 <21405@uktc-bg.com>
2026-03-19 01:41:55 +0200

    Add frontend - login, dashboard, router

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-transpileDependencies: true,
-lintOnSave: false
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    port: 8082,
+    proxy: {
+      '/auth': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      },
+      '/equipment': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      },
+      '/request': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      },
+      '/reports': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
+  }
 })

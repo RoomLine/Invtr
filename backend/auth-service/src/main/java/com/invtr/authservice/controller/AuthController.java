1ba7f448045253639cdb28f37cbc0f80f5aaa5f6
GamingDriver <yotov180@gmail.com>
2026-03-22 18:33:54 +0300

    removed dublicate file import

--- backend/auth-service/src/main/java/com/invtr/authservice/controller/AuthController.java
+++ backend/auth-service/src/main/java/com/invtr/authservice/controller/AuthController.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import com.invtr.authservice.service.AuthService;
-
 import java.util.List;
 
 @RestController
